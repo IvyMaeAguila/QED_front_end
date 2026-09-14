1c5c11ee3376964b3ceb2a02caf1d048bb8a3e86
Ivy <ivymae.aguila06@gmail.com>
2026-09-14 21:28:35 +0800

    Resolve merge conflicts from pull and stash

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,6 +1,9 @@
+<<<<<<< Updated upstream
 const API_BASE_URL ='http://localhost:7400';
 // const API_BASE_URL ='https://thesis-qed-1.onrender.com';
 
 export const API_CONFIG = {
   baseURL: API_BASE_URL
 };
+=======
+>>>>>>> Stashed changes
